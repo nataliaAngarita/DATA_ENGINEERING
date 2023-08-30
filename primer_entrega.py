89db5f28c0b19c95a2c71aeeb723c7f08b53317e
Natalia Angarita <buitragonatalia778@gmail.com>
2023-08-29 21:27:00 -0500

    primer entrega

--- primer_entrega.py
+++ primer_entrega.py
@@ -7,6 +7,7 @@ response = requests.get(url)
 if response.status_code == 200:  
     data = response.json()  
     for e in data['data']:
+     print(e['type'])
      print(e['title'])
 else:
     print("Error en la solicitud:", response.status_code)
